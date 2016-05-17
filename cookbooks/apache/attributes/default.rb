48273ee22ad21a4d80f7d6b818a1db44c0c801c1
root <root@ananda142.mylabserver.com>
2016-05-17 21:21:10 +0000

    my first commit chef-repo

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["ananda142"] = { "port" => 80, "domain" => "ananda142.mylabserver.com"}
+default["apache"]["sites"]["ananda143"] = { "port" => 80, "domain" => "ananda143.mylabserver.com"}
